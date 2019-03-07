cbee3f212b1a166f69b297e5ccb90d995595cfd2
Piotr Skoczylas <piotr.skoczylas@billennium.com>
2019-03-07 15:20:30 +0100

    Project skeleton.

---
+++ src/main/java/com/billennium/bcvsaver/BcvSaverApplication.java
@@ -0,0 +1,13 @@
+package com.billennium.bcvsaver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BcvSaverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BcvSaverApplication.class, args);
+    }
+
+}
